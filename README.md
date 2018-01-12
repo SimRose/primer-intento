f6c950b85ea002d7bdd57fb9f9727112b37e2f57
SimRose <nikko_6_6_6@hotmail.it>
2018-01-12 00:44:47 -0300

    Podes intentarlo hasta 5 veces

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # primer-intento
+
+"El programa pregunta el resultado de una suma y te permite intentar resolverla hasta 5 veces."
--- test_input.py
+++ test_input.py
@@ -1,16 +1,39 @@
+numero_correcto = 5
 numero_del_usuario = int(input("¿Cuanto es 3+2? "))
-if numero_del_usuario == 5:
+if numero_del_usuario == numero_correcto:
     print("Correcto")
     print("¡Hasta la próxima!")
 else:
-    print("Falso")
-    int(input("¿Cuanto es 3+2? "))
-    if numero_del_usuario == 5:
+    print("Incorrecto")
+    print("¡Intenta otra vez!")
+    numero_2_del_usuario = int(input("¿Cuanto es 3+2? "))
+    if numero_2_del_usuario == numero_correcto:
         print("Correcto")
         print("¡Hasta la próxima!")
     else:
-        print("Falso")
-    print("¡Volvé al colegio!")
+        print("Incorrecto")
+        print("¡Intenta otra vez!")
+        numero_3_del_usuario = int(input("¿Cuanto es 3+2? "))
+        if numero_3_del_usuario == numero_correcto:
+            print("Correcto")
+            print("¡Hasta la próxima!")
+        else:
+            print("Incorrecto")
+            print("¡Intenta otra vez!")
+            numero_4_del_usuario = int(input("¿Cuanto es 3+2? "))
+            if numero_4_del_usuario == numero_correcto:
+                print("Correcto")
+                print("¡Hasta la próxima!")
+            else:
+                print("Incorrecto")
+                print("¡Intenta otra vez!")
+                numero_5_del_usuario = int(input("¿Cuanto es 3+2? "))
+                if numero_5_del_usuario == numero_correcto:
+                    print("Correcto")
+                    print("¡Hasta la próxima!")
+                else:
+                    print("Incorrecto")
+                    print("¡Has perdido!")
 
 
 
